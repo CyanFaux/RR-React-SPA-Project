551190ea78571f9f747cc3ce67a8e651a847de0e
CyanFaux <santafox26@gmail.com>
2024-03-10 09:03:48 -0400

    added github logo

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,15 @@
 import React from "react";
+import GitHubLogo from "./images/github-mark.png"
 
 function Contact() {
-  return <div>
-    <h2>Contact Me</h2>
-  </div>;
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <a href="https://github.com/CyanFaux">
+        <img src={GitHubLogo} alt="GitHub Logo" style={{ width: "3rem", height: "3rem" }}/>
+      </a>
+    </div>
+  );
 }
 
 export default Contact;
